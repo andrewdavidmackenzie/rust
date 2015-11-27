cb0b2095120e03071971e4cb9eefb2a5ea3e52a7
Andrew Mackenzie <andrew@mackenzie-serres.net>
2015-11-27 22:56:20 +0100

    updated

--- philosophers/src/main.rs
+++ philosophers/src/main.rs
@@ -1,32 +1,69 @@
+use std::thread;
+use std::sync::{Mutex, Arc};
+
 struct Philosopher {
-name: String,
+	name: String,
+	left: usize,
+	right: usize,
+}
+
+struct Table {
+	forks: Vec<Mutex<()>>,
 }
 
 impl Philosopher {
-// Associated function
-fn new(name: &str) -> Philosopher {
-	Philosopher {
-	name: name.to_string(),
+	// Associated function
+	fn new(name: &str, left: usize, right: usize) -> Philosopher {
+		Philosopher {
+		name: name.to_string(),
+		left: left,
+		right: right,
+		}
+	}
+
+	// Methods take an explicit self parameter
+	fn eat(&self, table: &Table) {
+		let _left = table.forks[self.left].lock().unwrap();
+		let _right = table.forks[self.right].lock().unwrap();
+
+		println!("{} is eating.", self.name);
+
+		thread::sleep_ms(1000);
+
+		println!("{} is done eating.", self .name);
+
+		// Locks will be freed when the variables holding them (_left, _right)
+		// go out of scope, automatically
 	}
 }
 
-// Methods take an explicit self parameter
-fn eat(& self) {
-	println!("{} is done eating.", self .name);
-}
-}
-
 fn main() {
+	let table = Arc::new(Table { forks: vec![
+		Mutex::new(()),
+		Mutex::new(()),
+		Mutex::new(()),
+		Mutex::new(()),
+		Mutex::new(()),
+	]});
+
 	// Vec<T> is a growable array type
 	let philosophers = vec![
-	Philosopher::new("Judith Butler"),
-	Philosopher::new("Gilles Deleuze"),
-	Philosopher::new("Karl Marx"),
-	Philosopher::new("Emma Goldman"),
-	Philosopher::new("Michael Foucalt"),
+	Philosopher::new("Judith Butler", 0, 1),
+	Philosopher::new("Gilles Deleuze", 1, 2),
+	Philosopher::new("Karl Marx", 2, 3),
+	Philosopher::new("Emma Goldman", 3, 4),
+	Philosopher::new("Michael Foucalt", 0, 4),
 	];
 
-	for p in &philosophers {
-			p.eat();
+	let handles: Vec<_> = philosophers.into_iter().map( |p| {
+		let table = table.clone();
+
+		thread::spawn(move || {
+			p.eat(&table);
+		})
+	}).collect();
+
+	for h in handles {
+		h.join().unwrap();
 	}
 }
