5d72dee0ecfc396b80179fdc51794cbe80d0cea6
Andrew Mackenzie <andrew@mackenzie-serres.net>
2015-12-13 17:54:25 +0100

    Add a Drop implementation for List as the default one is not tail-recursive.

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -44,3 +44,57 @@ impl List {
         }
     }
 }
+
+/*
+    A non-tail recursive Drop deallocator for List, as it has a Boxed Node
+
+    This would be the basic code:
+
+    impl Drop for Box<Node> {
+    fn drop(&mut self) {
+        self.ptr.drop(); // uh oh, not tail recursive!
+        deallocate(self.ptr);
+    }
+}
+*/
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);    }
+}
